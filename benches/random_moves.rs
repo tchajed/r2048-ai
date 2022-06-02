0215ebcb7df1d21c2fd273ce81e66cd15c6febfa
Tej Chajed <tchajed@mit.edu>
2022-06-02 16:21:51 -0400

    Fix benchmark seed

--- benches/random_moves.rs
+++ benches/random_moves.rs
@@ -4,7 +4,7 @@ use rand::{prelude::StdRng, SeedableRng};
 
 fn random_game() -> u32 {
     let mut mgr = StateManager::from_rng(StdRng::seed_from_u64(0));
-    let mut move_rng = StdRng::seed_from_u64(1);
+    let mut move_rng = StdRng::seed_from_u64(2);
     // run for exactly 100 iterations so timing is easy to interpret
     for _ in 0..100 {
         if let Some((_, s)) = rand_move(&mgr.state(), &mut move_rng) {
