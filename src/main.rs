1e3902bc749f36db9c56adc34cc4ecbdedf73b47
Tej Chajed <tchajed@mit.edu>
2022-06-02 11:48:13 -0400

    Print number of moves made

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use r2048_ai::StateManager;
 
 fn main() {
     let mut mgr = StateManager::new();
+    let mut i = 0;
     print!("{}", mgr.state());
     loop {
         if let Some((m, s)) = rand_move(&mgr.state(), mgr.rng()) {
             println!("{:?}", m);
             mgr.next_state(s);
             print!("{}", mgr.state());
+            i += 1;
         } else {
             break;
         }
     }
-    println!("score: {}", mgr.state().score());
+    println!("score: {}  moves: {i}", mgr.state().score());
 }
