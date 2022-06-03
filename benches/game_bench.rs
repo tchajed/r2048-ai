2b14d718032a20e08ae82f947da1b9d05c719ea0
Tej Chajed <tchajed@mit.edu>
2022-06-02 21:00:22 -0400

    Add a benchmark for scoring

--- benches/game_bench.rs
+++ benches/game_bench.rs
@@ -1,5 +1,7 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use r2048_ai::{ai::rand_move, StateManager};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use r2048_ai::ai::{rand_move, sum_tiles_score, weight_score};
+use r2048_ai::game::State;
+use r2048_ai::StateManager;
 use rand::{prelude::StdRng, SeedableRng};
 
 fn random_game() -> u32 {
@@ -16,8 +18,20 @@ fn random_game() -> u32 {
     return mgr.state().highest_tile();
 }
 
+fn test_state() -> State {
+    let mut s = State::default();
+    for i in 0..16 {
+        s.set(i, (i % 4) as u8);
+    }
+    s
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("random game", |b| b.iter(|| random_game()));
+
+    let s = test_state();
+    c.bench_function("sum tiles", |b| b.iter(|| sum_tiles_score(&black_box(s))));
+    c.bench_function("weight score", |b| b.iter(|| weight_score(&black_box(s))));
 }
 
 criterion_group!(benches, criterion_benchmark);
