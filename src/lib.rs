6217e1a9f39d84721583bf97190399816b4a916e
Tej Chajed <tchajed@mit.edu>
2022-06-03 08:03:49 -0400

    Extract config to separate implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,91 @@
+use std::time::Instant;
+
+use ai::{expectimax_sum_move, expectimax_weight_move, rand_move, smart_depth};
+use game::{Move, State};
+use rand::{prelude::ThreadRng, Rng};
+
+use crate::game::Game;
+
 #[macro_use]
 extern crate static_assertions;
 
 pub mod ai;
 pub mod game;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Depth {
+    Smart,
+    Fixed(u32),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Algorithm {
+    ExpectimaxSum(Depth),
+    ExpectimaxWeight(Depth),
+    Random,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Config {
+    pub algorithm: Algorithm,
+    pub target_score: Option<u32>,
+}
+
+impl Config {
+    fn choose_depth(d: Depth, s: &State) -> u32 {
+        match d {
+            Depth::Smart => smart_depth(s),
+            Depth::Fixed(d) => d,
+        }
+    }
+
+    fn next_move(&self, s: &State) -> Option<(Move, State)> {
+        match self.algorithm {
+            Algorithm::ExpectimaxSum(d) => expectimax_sum_move(s, Self::choose_depth(d, s)),
+            Algorithm::ExpectimaxWeight(d) => expectimax_weight_move(s, Self::choose_depth(d, s)),
+            Algorithm::Random => rand_move(s, &mut ThreadRng::default()),
+        }
+    }
+
+    /// Run runs the game and returns a score and whether or not this is a win.
+    pub fn run(&self) -> bool {
+        let mut mgr = Game::new();
+        print!("{}", mgr.state());
+        let start = Instant::now();
+        while let Some((m, s)) = self.next_move(mgr.state()) {
+            mgr.next_state(s);
+
+            let moves = mgr.moves();
+            if moves % 25 == 0 {
+                let elapsed_s = start.elapsed().as_secs_f64();
+                let moves_per_s = moves as f64 / elapsed_s;
+                println!("  {:>4} {:?} {:0.0} moves/s", moves, m, moves_per_s);
+            } else {
+                println!("  {:>4} {:?}", mgr.moves(), m);
+            }
+            print!("{}", mgr.state());
+            if let Some(target) = self.target_score {
+                if mgr.state().highest_tile() == target {
+                    break;
+                }
+            }
+        }
+        let elapsed_s = start.elapsed().as_secs_f64();
+        let moves_per_s = mgr.moves() as f64 / elapsed_s;
+        println!(
+            "score: {score}  moves: {moves}  {moves_per_s:0.0} moves/s",
+            score = mgr.state().highest_tile(),
+            moves = mgr.moves(),
+            moves_per_s = moves_per_s,
+        );
+        return self.won(&mgr);
+    }
+
+    pub fn won<R: Rng>(&self, g: &Game<R>) -> bool {
+        if let Some(target) = self.target_score {
+            g.state().highest_tile() >= target
+        } else {
+            true
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
-use std::time::Instant;
+use std::process;
 
-use r2048_ai::{
-    ai::{expectimax_weight_move, smart_depth},
-    game::Game,
-};
+use r2048_ai::{Algorithm, Config, Depth};
 
 fn main() {
-    let mut mgr = Game::new();
-    print!("{}", mgr.state());
-    let start = Instant::now();
-    while let Some((m, s)) = expectimax_weight_move(mgr.state(), smart_depth(mgr.state())) {
-        mgr.next_state(s);
-
-        let moves = mgr.moves();
-        if moves % 25 == 0 {
-            let elapsed_s = start.elapsed().as_secs_f64();
-            let moves_per_s = moves as f64 / elapsed_s;
-            println!("  {:>4} {:?} {:0.0} moves/s", moves, m, moves_per_s);
-        } else {
-            println!("  {:>4} {:?}", mgr.moves(), m);
-        }
-        print!("{}", mgr.state());
-        if mgr.state().highest_tile() == 2048 {
-            break;
-        }
+    let config = Config {
+        algorithm: Algorithm::ExpectimaxWeight(Depth::Smart),
+        target_score: Some(2048),
+    };
+    let win = config.run();
+    if !win {
+        eprintln!("failed to get to {}", config.target_score.unwrap());
+        process::exit(1);
     }
-
-    let elapsed_s = start.elapsed().as_secs_f64();
-    let moves_per_s = mgr.moves() as f64 / elapsed_s;
-    println!(
-        "score: {score}  moves: {moves}  {moves_per_s:0.0} moves/s",
-        score = mgr.state().highest_tile(),
-        moves = mgr.moves(),
-        moves_per_s = moves_per_s,
-    );
 }
