49b839a8c8edec3d0517c75180514eb1c826506e
Tej Chajed <tchajed@mit.edu>
2022-06-03 08:18:23 -0400

    Add a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+use clap::Parser;
 use std::process;
 
 use r2048_ai::{Algorithm, Config, Depth};
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long, default_value = "weight")]
+    algorithm: String,
+
+    #[clap(short, long)]
+    depth: Option<u32>,
+
+    #[clap(short, long, default_value_t = 2048)]
+    score: u32,
+
+    #[clap(short, long)]
+    unbounded: bool,
+}
+
 fn main() {
+    let args = Args::parse();
+    let depth = match args.depth {
+        Option::Some(d) => Depth::Fixed(d),
+        Option::None => Depth::Smart,
+    };
+    let algorithm = if args.algorithm == "weight" {
+        Algorithm::ExpectimaxWeight(depth)
+    } else if args.algorithm == "sum" {
+        Algorithm::ExpectimaxSum(depth)
+    } else if args.algorithm == "random" {
+        Algorithm::Random
+    } else {
+        eprintln!("unknown algorithm {}", args.algorithm);
+        process::exit(1);
+    };
+    let target_score = if args.unbounded {
+        None
+    } else {
+        Some(args.score)
+    };
+
     let config = Config {
-        algorithm: Algorithm::ExpectimaxWeight(Depth::Smart),
-        target_score: Some(2048),
+        algorithm,
+        target_score,
     };
     let win = config.run();
     if !win {
